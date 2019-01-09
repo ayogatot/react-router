f49f1a03429d187e65ed4cacc80e7a3d0ebcf0c3
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2019-01-09 13:04:34 +0700

    create home page

---
+++ src/components/Home/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+// import styled from "styled-components";
+
+import Main from "../Main";
+
+class Home extends Component {
+  render() {
+    return (
+        <Main>
+            <p>This is Main Page</p>
+        </Main>
+    );
+  }
+}
+
+export default Home;
