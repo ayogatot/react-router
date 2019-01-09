daa9a4a2be0204286db08d95d61eea4a8e1df0e0
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2019-01-09 13:04:43 +0700

    create about page

---
+++ src/components/About/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+// import styled from "styled-components";
+
+import Main from "../Main";
+
+class About extends Component {
+    render() {
+        return (
+            <Main>
+                <p>This is About Page</p>
+            </Main>
+        );
+    }
+}
+
+export default About;
