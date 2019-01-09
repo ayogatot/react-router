52e187927050a62c6ef7abb262f48979eb0ba261
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2019-01-09 13:04:56 +0700

    create news page

---
+++ src/components/News/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+// import styled from "styled-components";
+
+import Main from "../Main";
+
+class News extends Component {
+  render() {
+    return (
+      <Main>
+        <p>This is News Page</p>
+      </Main>
+    );
+  }
+}
+
+export default News;
